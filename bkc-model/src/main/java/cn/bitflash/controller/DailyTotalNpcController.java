3a21d8cd12286fdfee9c7f24b1aa7a098131b8ce
chen <chenchengyi@bitflash.vip>
2018-10-16 18:03:42 +0800

    tj

--- bkc-model/src/main/java/cn/bitflash/controller/DailyTotalNpcController.java
+++ bkc-model/src/main/java/cn/bitflash/controller/DailyTotalNpcController.java
@@ -2,6 +2,8 @@ package cn.bitflash.controller;
 
 import cn.bitflash.entity.DailyTotalNpcEntity;
 import cn.bitflash.service.DailyTotalNpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,14 +16,16 @@ import java.util.Date;
 @RestController
 public class DailyTotalNpcController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private DailyTotalNpcService dailyTotalNpcService;
 
     @PostMapping("/inner/dailytotalnpc/selectById")
-    public DailyTotalNpcEntity selectById(@RequestParam("id")LocalDate id){
-        System.out.println("------------------------------------------------------");
-        System.out.println("当前系统时间为-----------"+id+"-----------------");
-        System.out.println("------------------------------------------------------");
+    public DailyTotalNpcEntity selectById(@RequestParam("id")String id){
+        logger.info("------------------------------------------------------");
+        logger.info("当前系统时间为-----------" + id + "-----------------");
+        logger.info("------------------------------------------------------");
         return dailyTotalNpcService.selectById(id);
     }
 
