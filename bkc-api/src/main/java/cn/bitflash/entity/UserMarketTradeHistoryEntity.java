b17ee610089f0021900472e457f4ad06f07e311e
wj <wj@bitflash.vip>
2018-10-18 15:26:37 +0800

    提交代码

--- bkc-api/src/main/java/cn/bitflash/entity/UserMarketTradeHistoryEntity.java
+++ bkc-api/src/main/java/cn/bitflash/entity/UserMarketTradeHistoryEntity.java
@@ -23,10 +23,10 @@ public class UserMarketTradeHistoryEntity implements Serializable {
     //Y:已查看,N未查看
     private String isRead;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss" )
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss" )
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date finishTime;
 
     public String getUserTradeId() {
