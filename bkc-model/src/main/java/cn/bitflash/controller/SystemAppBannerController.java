2c33f559dcc14fd163cc7648987f91aebeae6c01
chen <chenchengyi@bitflash.vip>
2018-10-29 09:54:06 +0800

    tj

--- bkc-model/src/main/java/cn/bitflash/controller/SystemAppBannerController.java
+++ bkc-model/src/main/java/cn/bitflash/controller/SystemAppBannerController.java
@@ -1,15 +1,21 @@
 package cn.bitflash.controller;
 
+import cn.bitflash.bean.ImgForm;
 import cn.bitflash.entity.SystemAppBannerEntity;
+import cn.bitflash.entity.UserSecretEntity;
 import cn.bitflash.service.SystemAppBannerService;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import sun.misc.BASE64Decoder;
 
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author GAOYGUUO
@@ -73,5 +79,46 @@ public class SystemAppBannerController {
     public List<SystemAppBannerEntity> selectAppBanner() {
         return systemAppBannerService.selectAppBanner();
     }
+
+    @PostMapping("/inner/systemAppBanner/mobileBannerSetting")
+    public Map<String,Object> uploadPayment(@RequestBody ImgForm imgForm) {
+        String path = "/home/statics/banner/";
+        String imgName = RandomStringUtils.randomAlphanumeric(10);
+        path = path+imgName;
+        BASE64Decoder decoder = new BASE64Decoder();
+        try {
+            // Base64解码
+            String[] base64Str = imgForm.getImg().split(",");
+            if (base64Str.length >= 2) {
+                byte[] b = decoder.decodeBuffer(base64Str[1]);
+                for (int i = 0; i < b.length; ++i) {
+                    if (b[i] < 0) {// 调整异常数据
+                        b[i] += 256;
+                    }
+                }
+                // 生成jpeg图片
+                OutputStream out = new FileOutputStream(path);
+                out.write(b);
+                out.flush();
+                out.close();
+            } else {
+                return new ModelMap("status",500);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ModelMap("status",500);
+        }
+
+        SystemAppBannerEntity appBannerEntity = new SystemAppBannerEntity();
+        appBannerEntity.setId(imgForm.getId());
+        appBannerEntity.setBannerAddress(imgName);
+        appBannerEntity.setCreateTime(new Date());
+        appBannerEntity.setOpendAddress(imgForm.getOpenAddress());
+        appBannerEntity.setTitle(imgForm.getTitle());
+        systemAppBannerService.insertOrUpdate(appBannerEntity);
+        return new ModelMap("status",0);
+    }
+
+
 
 }
